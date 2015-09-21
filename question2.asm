8ee38a6f6151a7bf549a20739ab3272153731e56
barraod <odonoghuebarra@gmail.com>
2015-09-21 13:44:51 +0100

    Update 21/9/15 All programs tested and working

--- question2.asm
+++ question2.asm
@@ -5,22 +5,17 @@ D1: 	.word 1,0,3,5,0,4,8,9
 main: 	la $t0, D1
 	li $t1, 0 #initialize to zero
 	li $t2, 7 #count
-Num:	addi $t2, $t2, 0
-	addi $t3, $t2, 0 #position
+Num:	addi $t3, $t2, 0 #position
 	lw $t5, 0($t0)
-	beq $t5, $zero, END
-	#beq $t3, $zero, END
-Mult:	
-	sll $t6, $t5, 3
-	sll $t7, $t5, 1
-	add $t5, $t6, $t7
+	beq $t5, $zero, ENDFOR
+Mult:	sll $t6, $t5, 3		# Multiply by 10^n where n is its position from the least significant digit
+	sll $t7, $t5, 1		# This uses left bit shifting of n by 3 and 1 bits, then adds the results to make 10*n.
+	add $t5, $t6, $t7	# This can be repeated as many times as necessary using a loop.
 	addi $t3, $t3, -1
-	#beq $t2, $zero, END
 	bne $t3, $zero, Mult
-END:	add $t1, $t1, $t5
+ENDFOR:	add $t1, $t1, $t5
 	addi $t0, $t0, 4
 	addi $t2, $t2, -1
-	addi $t3, $t2, -1 #position
 	bne $t2, $zero, Num
 	lw $t5, 0($t0)
 	add $t1, $t1, $t5
